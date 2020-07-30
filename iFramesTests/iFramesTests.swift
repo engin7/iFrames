7cf3c37ced4edfb7bc3d590a49e5259491f3e5b4
Engin KUK <kuk.engin@gmail.com>
2020-07-31 01:28:42 +0300

    Initial Commit

---
+++ iFramesTests/iFramesTests.swift
@@ -0,0 +1,34 @@
+//
+//  iFramesTests.swift
+//  iFramesTests
+//
+//  Created by Engin KUK on 31.07.2020.
+//  Copyright © 2020 Engin KUK. All rights reserved.
+//
+
+import XCTest
+@testable import iFrames
+
+class iFramesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
